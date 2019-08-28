30c2f3a1d8f52be35f39f662e9c55aed0ac906bf
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-28 19:37:44 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  array = []
+  new_array = []
   counter = 0
   while counter < array.length do
-    
+    new_array.push(array[counter] * array[counter])
     counter += 1
   end
-  square_array(array)
+  new_array
 end
